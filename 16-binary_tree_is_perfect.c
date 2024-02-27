b870dd882d08a847423f86721a25945cd92fe7b0
tarekhdeen <tarekhdeen@gmail.com>
2024-02-27 13:01:56 -0800

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,7 +4,7 @@ unsigned char is_leaf(const binary_tree_t *node);
 size_t depth(const binary_tree_t *tree);
 const binary_tree_t *get_leaf(const binary_tree_t *tree);
 int is_perfect_recursive(const binary_tree_t *tree,
-								 size_t leaf_depth, size_t level);
+size_t leaf_depth, size_t level);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 
 /**
@@ -74,4 +74,3 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	return (is_perfect_recursive(tree, depth(get_leaf(tree)), 0));
-g
